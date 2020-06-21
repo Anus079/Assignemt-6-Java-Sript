e0c9291ea2fba54eef07eff37c83db369fddb28f
Anus079 <anuszahid0798@gmail.com>
2020-06-21 15:55:36 +0500

    Java-script assignment#6

---
+++ app.js
@@ -0,0 +1,70 @@
+//  Chapter # 21-25 ...
+
+// Task # 1...
+
+    // var firstname  = prompt("Enter your first name");
+    // var lastname  = prompt("Enter your last name");
+    // var fullname = firstname + lastname;
+    // alert("Welcome  " + fullname)
+
+//  Task # 2...
+
+    // var Faviortemobile = prompt("What is your Faviorte mobile phone model");
+
+    // console.log(Faviortemobile.length)
+
+// Task # 3...
+
+    // var country = "Pakistani";
+    // document.write(country.indexOf("n"));
+
+
+//  Task # 4 ...
+
+    // var string = "Hello World";
+    // var index;
+    // document.write( "String" + ":" + string+ "<br> <br>" + "Last Index of l" + ":" + string.lastIndexOf("l") )
+    
+    
+//  Task # 5 ...
+    
+    // var string = "Pakistani";
+    
+    // document.write( "String" + ":" + string+ "<br> <br>" + "Characters at index 3" + ":" + string.charAt(3) )
+    
+
+// Task # 6 . .. 
+
+    // var firstname  = prompt("Enter your first name");
+    // var lastname  = prompt("Enter your last name");
+    // var fullname = firstname.concat(lastname);
+    // console.log(fullname)
+
+// Task # 7 ... 
+
+// var city = "hyderabad";
+// var newcity =city.replace("hyder","Islam");
+// console.log(newcity)
+
+//  Task # 8 ...
+
+
+    // var message = "Ali and Sami are best friends. They play cricket and football together";
+    // var repalce = message.replace(/and/g,"&")
+    // console.log(repalce)
+
+// Task # 9...
+
+
+    // var string = "472";
+    // var number = parseInt(string)
+    // console.log(number)
+
+// Task # 10 ...
+
+    // var input = prompt("Enter something and changed into upper case letter");
+    // var change = input.toUpperCase(input)
+    // console.log(change)
+
+//  Task # 11 ...
+
